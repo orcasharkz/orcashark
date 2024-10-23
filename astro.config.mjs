4767d224fd5cb79c41e3ad7f70cf93c351cbaf4a
高輔 <wolftiger.orcashark@gmail.com>
2024-10-24 00:56:09 +0900

    ➕ mdxとGoogle Fonts(M PLUS1)追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
-	site: "https://orcasharkz.github.io",
-	base: "/orcashark",
-	server: {
-		open: true,
+  site: "https://orcasharkz.github.io",
+  base: "/orcashark",
+
+  server: {
+      open: true,
 	},
+
+  integrations: [mdx()],
 });
